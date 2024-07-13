2de8e8f86f138f7e22fe9698f3f72f6be5359442
nekotori55 <nekotori55@gmail.com>
2024-07-13 20:16:06 +0300

    Вынос обновления расписания в отдельную функцию

--- schedule/main.go
+++ schedule/main.go
@@ -43,76 +43,61 @@ func main() {
 	defer db.Close(context.Background())
 
 	app.Get("/schedule/update/", func(c *fiber.Ctx) error {
-		coursesCount := len(getCourses())
-
-		output := ""
-
-		var batch pgx.Batch
-
-		for course := 0; course < coursesCount; course++ {
-			groups := Values(getGroups(course))
-
-			for _, group := range groups {
-				schedule := getSchedule(course, group)
-
-				// optional
-				// time.Sleep(100 * time.Millisecond)
-
-				for _, daySchedule := range schedule {
-					var lessons = parseDaySchedule(daySchedule, group)
-
-					if lessons == nil {
-						continue
-					}
-
-					for _, lesson := range lessons {
-
-						batch.QueuedQueries = append(batch.QueuedQueries, &pgx.QueuedQuery{
-							SQL: `INSERT INTO schedule(` +
-								`title, "group",` +
-								`subgroup, building, ` +
-								`"type", room, ` +
-								`professors, notes, ` +
-								`regularity, "day", ` +
-								`"index"` +
-								`) VALUES (` +
-								`$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11` +
-								`)`,
-							Arguments: []any{
-								lesson.Title, lesson.Group,
-								lesson.Subgroup, lesson.Building,
-								lesson.Type, lesson.Room,
-								lesson.Professors, lesson.Notes,
-								lesson.Regularity, lesson.Day,
-								lesson.Index,
-							},
-						})
-					}
-				}
-			}
-		}
-
-		db.Exec(context.Background(), "TRUNCATE TABLE schedule")
-
-		results := db.SendBatch(context.Background(), &batch)
-		err := results.Close()
-
-		if err != nil {
-			panic(err.Error())
-		}
-
+		output := updateSchedule()
 		return c.SendString(output)
-	})
+	}) 
 
 	app.Get("/schedule/get", func(c *fiber.Ctx) error {
 		filters := c.Queries()
-
 		lessons := getLessons(filters)
-
 		return c.JSON(lessons)
 	})
 
 	log.Fatal(app.Listen(":3000"))
+}
+
+func updateSchedule() string {
+	output := ""
+	var batch pgx.Batch
+
+	courses := getCourses()
+	for courseIndex := range courses {
+		groups := Values(getGroups(courseIndex))
+
+		for _, group := range groups {
+			schedule := getSchedule(courseIndex, group)
+
+			for _, daySchedule := range schedule {
+				var lessons = parseDaySchedule(daySchedule, group)
+
+				for _, lesson := range lessons {
+
+					batch.QueuedQueries = append(batch.QueuedQueries, &pgx.QueuedQuery{
+						SQL: `INSERT INTO schedule(` +
+							`title, "group", subgroup, building, ` +
+							`"type", room, professors, notes, ` +
+							`regularity, "day", "index"` +
+							`) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)`,
+						Arguments: []any{
+							lesson.Title, lesson.Group, lesson.Subgroup, lesson.Building,
+							lesson.Type, lesson.Room, lesson.Professors, lesson.Notes,
+							lesson.Regularity, lesson.Day, lesson.Index,
+						},
+					})
+				}
+			}
+		}
+	}
+
+	db.Exec(context.Background(), "TRUNCATE TABLE schedule")
+
+	results := db.SendBatch(context.Background(), &batch)
+	err := results.Close()
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return output
 }
 
 func getLessons(filters map[string]string) []lesson {
