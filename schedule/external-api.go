dbfae58982fa00d9ccc33088c75b26583155fca9
nekotori55 <nekotori55@gmail.com>
2024-07-13 20:36:20 +0300

    Изменение названия файла

--- schedule/external-api.go
+++ schedule/external-api.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+
 func getSchedule(course int, group string) map[string][]string {
 	statusCode, body := getJSONFromURI(apiPath + "/getschedule/" + fmt.Sprint(course) + "/" + group)
 
