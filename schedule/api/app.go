970c377210211f0ce7b2159b24846a95a1809b39
Nekotori <nekotori55@gmail.com>
2024-08-07 20:46:23 +0300

    Добавление эндпоинта для документации

--- schedule/api/app.go
+++ schedule/api/app.go
@@ -8,6 +8,7 @@ import (
 	routes_v1 "kb-bmstu-map-api/schedule/api/domain/v1/routes"
 	usecases_v1 "kb-bmstu-map-api/schedule/api/domain/v1/usecases"
 
+	swagger "github.com/arsmn/fiber-swagger/v2"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -26,6 +27,16 @@ func main() {
 	// API Group Versions
 	v1 := app.Group("/v1")
 	routes_v1.ScheduleRouter(v1, scheduleUsecaseV1)
+
+	app.Static("/swagger/", "./docs/")
+	swaggerConfig := swagger.Config{
+		DisplayRequestDuration: true,
+	}
+
+	config_v1 := swaggerConfig
+	config_v1.URL = "/swagger/v1/swagger.json"
+
+	v1.Get("/docs/*", swagger.New(config_v1))
 
 	log.Fatal(app.Listen(":3000"))
 }
