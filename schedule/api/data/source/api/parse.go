774010050729888101a26f500e6699c18687a740
Nikita Kotenko <nikita_kot2@mail.ru>
2024-08-27 16:41:53 +0300

    Добавил новое здание и фикс знака для УАК
    Встретилось УАК_1 а не УАК-1

--- schedule/api/data/source/api/parse.go
+++ schedule/api/data/source/api/parse.go
@@ -13,8 +13,9 @@ var scheduleParseExp = regexp2.MustCompile(``+
 	`(?P<location>`+
 	/**/ `(?P<building>\d)[-_](?P<room>\d{3}[а-яё]?(?:[\/\d]{2})?)`+
 	/**/ `|(?P<building>к\.\d лаб.)`+
-	/**/ `|(?P<building>УАК\d)-(?P<room>\d.\d{2})`+
-	/**/ `|(?P<building>НПП "Тайфун"|ООО РИТЦ|ОКБ "МЭЛ"|Спортзал)) ?`+
+	/**/ `|(?P<building>к\.\d)` +
+	/**/ `|(?P<building>УАК\d)(-|_)(?P<room>\d.\d{2})`+
+	/**/ `|(?P<building>НПП "Тайфун"|ООО РИТЦ|ОКБ "МЭЛ"|Спортзал|ПАО "КАДВИ")) ?`+
 	`(?P<professors>(?:(?:[А-ЯЁ][а-яё]+)+(?:, [А-ЯЁ][а-яё]+)*))? ?`+
 	`(?P<notes>[а-яё. ]+)? ?`,
 	regexp2.RE2)
